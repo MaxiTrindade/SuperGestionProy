a0994cb0ba907fd80d825b09457b1d796ae7ea9c
ramiromatt <ramiromatt@hotmail.com>
2015-06-01 18:46:37 -0300

    Header Clientes
    Prueba header clientes para comenzar a trabajar

---
+++ Headers/Clientes.h
@@ -0,0 +1,10 @@
+#ifndef CLIENTES_H_INCLUDED
+#define CLIENTES_H_INCLUDED
+
+class Clientes {
+    private:
+        int codCli;
+        char* nombre[20],apellido[20];
+};
+
+#endif // CLIENTES_H_INCLUDED
